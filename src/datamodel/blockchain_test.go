b67b74323fb42518fa87420e2c29602d8af2c021
ntefa <andrea.batino94@gmail.com>
2023-06-01 16:31:27 +0200

    added TestIsValidEncryption

--- src/datamodel/blockchain_test.go
+++ src/datamodel/blockchain_test.go
@@ -8,6 +8,18 @@ import (
 	"github.com/bwesterb/go-ristretto"
 )
 
+var _TestIsValidEncryption = struct {
+	name        string
+	amount      int64
+	wrongH      ristretto.Point
+	errorString string
+}{
+	name:        "Invalid encryption",
+	amount:      100,
+	wrongH:      pedersen.GenerateH(),
+	errorString: "encryption not valid",
+}
+
 func TestCreateAccount(t *testing.T) {
 	// Set up dependencies
 	var b Blockchain
@@ -23,5 +35,22 @@ func TestCreateAccount(t *testing.T) {
 	// Verify the output
 	if account.CommittedBalance != expectedCommittedBalance {
 		t.Errorf("Unexpected committedBalance value. Got: %v, Expected: %v", account.CommittedBalance, expectedCommittedBalance)
+	}
+}
+
+func TestIsValidEncryption(t *testing.T) {
+	var b Blockchain
+	var vX ristretto.Scalar
+	b.Init()
+	testCase := _TestIsValidEncryption
+
+	wrongCommittedAmount := pedersen.CommitTo(&testCase.wrongH, &b.BindingFactor, vX.SetBigInt(big.NewInt(testCase.amount)))
+
+	// Call the function being tested
+	err := b.isValidEncryption(testCase.amount, wrongCommittedAmount)
+
+	// Verify the output
+	if err.Error() != testCase.errorString {
+		t.Fatalf("Expected %v, but got: %v", testCase.errorString, err.Error())
 	}
 }
